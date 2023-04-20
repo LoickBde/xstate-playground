f1197a93e067027a3abdcaf3722c41240437b14f
LoickBde <loick.baude@decathlon.com>
2023-04-20 12:16:34 +0200

    feat: implements context and assing

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -5,7 +5,7 @@ export const Todos = () => {
   const [stateTodos, sendTodos] = useMachine(todosMachine, {
     services: {
       loadTodos: async () => {
-        return ["Learn xtstate", "Drink beers"];
+        return ["Learn xstate", "Drink beers"];
       },
     },
   });
@@ -13,23 +13,7 @@ export const Todos = () => {
     <>
       <h2>Todos</h2>
       <p>{JSON.stringify(stateTodos.value)}</p>
-      <button
-        onClick={() =>
-          sendTodos({ type: "loadingTodosCompleted", todos: ["Learn xstate"] })
-        }
-      >
-        Todos Loading OK
-      </button>
-      <button
-        onClick={() =>
-          sendTodos({
-            type: "loadingTodosFailed",
-            errorMessage: "Error occured",
-          })
-        }
-      >
-        Todos Loading FAILED
-      </button>
+      <p>{JSON.stringify(stateTodos.context)}</p>
     </>
   );
 };
--- src/machines/todosMachine.ts
+++ src/machines/todosMachine.ts
@@ -1,37 +1,59 @@
-import { createMachine } from "xstate";
+import { createMachine, assign } from "xstate";
 
-export const todosMachine = createMachine({
-  id: "todos",
-  predictableActionArguments: true,
-  initial: "loadingTodos",
-  schema: {
-    // events: {} as
-    //   | { type: "loadingTodosCompleted"; todos: string[] }
-    //   | { type: "loadingTodosFailed"; errorMessage: string },
-    services: {} as {
-      loadTodos: {
-        data: string[];
-      };
-    },
-  },
-  tsTypes: {} as import("./todosMachine.typegen").Typegen0,
-  states: {
-    loadingTodos: {
-      invoke: {
-        src: "loadTodos",
-        onDone: [
-          {
-            target: "todosLoaded",
-          },
-        ],
-        onError: [
-          {
-            target: "todosFailed",
-          },
-        ],
+export const todosMachine = createMachine(
+  {
+    id: "todos",
+    predictableActionArguments: true,
+    tsTypes: {} as import("./todosMachine.typegen").Typegen0,
+    schema: {
+      // events: {} as
+      //   | { type: "loadingTodosCompleted"; todos: string[] }
+      //   | { type: "loadingTodosFailed"; errorMessage: string },
+      services: {} as {
+        loadTodos: {
+          data: string[];
+        };
       },
     },
-    todosLoaded: {},
-    todosFailed: {},
+    context: {
+      todos: [] as string[],
+      errorMessage: undefined as string | undefined,
+    },
+    initial: "loadingTodos",
+    states: {
+      loadingTodos: {
+        invoke: {
+          src: "loadTodos",
+          onDone: [
+            {
+              target: "todosLoaded",
+              actions: "assignTodosToContext",
+            },
+          ],
+          onError: [
+            {
+              target: "todosFailed",
+              actions: "assignErrorToContext",
+            },
+          ],
+        },
+      },
+      todosLoaded: {},
+      todosFailed: {},
+    },
   },
-});
+  {
+    actions: {
+      assignTodosToContext: assign((context, event) => {
+        return {
+          todos: event.data,
+        };
+      }),
+      assignErrorToContext: assign((context, event) => {
+        return {
+          errorMessage: (event.data as Error).message,
+        };
+      }),
+    },
+  }
+);
